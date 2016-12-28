562c4013e656726091bbb0be27b9e0d7bcd3d56c
DataNick <DataNick@users.noreply.github.com>
2016-12-28 04:03:50 -0500

    Add bind function to capture this value

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,6 +6,10 @@ export default Ember.Controller.extend({
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
 
+  headerMessage: 'Coming Soon',
+
+  responseMessage: '',
+
   emailAddress: '',
 
   actualEmailAddress: Ember.computed('emailAddress', function() { 
@@ -19,9 +23,14 @@ export default Ember.Controller.extend({
   actions: {
     saveInvitation() {
       console.log("Success!!");
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('invitation', { email: email });
+            newInvitation.save().then(function(response){
+              this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+              this.set('emailAddress', '');
+            }.bind(this));
       alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
     }
   }
 
