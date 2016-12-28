fea3b49ae240a174fa7c13de23ca5a7600a57007
DataNick <DataNick@users.noreply.github.com>
2016-12-28 04:42:54 -0500

    Modified saveInvitation action

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -26,10 +26,10 @@ export default Ember.Controller.extend({
       const email = this.get('emailAddress');
 
       const newInvitation = this.store.createRecord('invitation', { email: email });
-            newInvitation.save().then(function(response){
-              this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+            newInvitation.save().then((response) => {
+              this.set('responseMessage', `Thank you! We've just saved your email address with the following id: ${response.get('id')}`);
               this.set('emailAddress', '');
-            }.bind(this));
+            });
       alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
     }
   }
