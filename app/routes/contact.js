b01b4a040b8db83b1ffbd59d52a00d06d35989ce
DataNick <DataNick@users.noreply.github.com>
2016-12-30 00:00:55 -0500

    Move actions out of controller and into routes/contact.js

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return this.store.createRecord('contact');
+  },
+
+  actions: {
+    messageSent(){
+      console.log("Success!!");
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')} ${this.get('message')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')} ${this.get('message')}`);
+      this.set('emailAddress', '');
+      this.set('message', '');
+    },
+
+    saveContact(newContact) {
+      newContact.save().then((response) => {
+        this.refresh();
+      })
+    },
+
+    willTransition() {
+          let model = this.controller.get('model');
+
+          if (model.get('isNew')) {
+            model.destroyRecord();
+          }
+
+          this.controller.set('responseMessage', false);
+        }
+
+  }
+
 });
